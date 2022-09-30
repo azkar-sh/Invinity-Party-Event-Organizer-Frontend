48302bf6d44ca2d0bdafdc050bec589153676ec6
azkar-sh <akbar.dizir@gmail.com>
2022-09-30 14:19:40 +0700

    installing react

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import LandingPages from "./pages/LandingPages";
+import Signin from "./pages/Signin";
+import Signup from "./pages/Signup";
+import Notfound from "./pages/NotFound";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        {/* AUTH */}
+        <Route path="/signin" element={<Signin />} />
+        <Route path="/signup" element={<Signup />} />
+
+        {/* LANDING */}
+        <Route path="/" element={<LandingPages />} />
+
+        {/* NOT FOUND */}
+        <Route path="*" element={<Notfound />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
