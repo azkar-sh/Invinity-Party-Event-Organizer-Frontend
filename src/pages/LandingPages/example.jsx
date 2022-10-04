931bdcdf42f03107714374b30b76a360e39c240a
azkar-sh <akbar.dizir@gmail.com>
2022-10-04 09:35:31 +0700

    getItem token

---
+++ src/pages/LandingPages/example.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function Landing() {
+  return (
+    <div>
+      <div className="container d-flex gap-3">
+        <div className="card"></div>
+      </div>
+      <div className="d-flex gap-2 justify-content-center w-100 my-5">
+        <div className="btn btn-primary"></div>
+        <div className="btn btn-primary"></div>
+      </div>
+    </div>
+  );
+}
